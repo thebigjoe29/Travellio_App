ae5af730877c009068162f67e0303927f97474b5
warooon <91374407+warooon@users.noreply.github.com>
2023-11-05 00:28:37 +0530

    Login UI Fixed

---
+++ lib/app/screens/home/home_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:travellio/app/screens/home/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
